d2502f0f3ca6f757d94d70fe9139c6866f5027ec
maingrid <96063025+maingrid@users.noreply.github.com>
2023-06-09 20:55:08 +0300

    rewrite test checkFilterNewsPerDay

--- src/test/java/com/example/habrtest/MainPageTest.java
+++ src/test/java/com/example/habrtest/MainPageTest.java
@@ -9,9 +9,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -25,9 +26,6 @@ public class MainPageTest {
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-
-
     }
 
     @AfterEach
@@ -48,7 +46,7 @@ public class MainPageTest {
         ratingFilter.click();
         WebElement acceptButton = driver.findElement(By.xpath("//button[contains(@class,' btn_solid btn_small') and contains(text(),'Применить')]"));
         acceptButton.click();
-        assertTrue(driver.findElement(By.xpath("//time[@title='2023-06-03, 20:26']")).isDisplayed(), "нет такой даты");
+        assertTrue(driver.findElement(By.xpath("//article[@class='tm-articles-list__item'][1]")).isDisplayed(), "нет такой даты");
 
     }
 
@@ -57,7 +55,7 @@ public class MainPageTest {
         driver.get("https://www.habr.com/");
         WebElement companyRUVDS = driver.findElement(By.xpath("//a[@href='/ru/companies/ruvds/articles/']"));
         companyRUVDS.click();
-
+        System.out.println();
         assertTrue(driver.findElement(By.xpath("//a[@href='/ru/companies/ruvds/profile/']/parent::div[@class='tm-company-card__info']")).isDisplayed());
     }
 }
